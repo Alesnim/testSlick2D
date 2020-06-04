04c30753c2223b434012f701c01e75334693ac53
alesnim@gmail.com <alesnim@gmail.com>
2020-06-04 19:02:29 +0300

    add something

--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -1,64 +1,69 @@
 package com.javalesson.oop;
 
 import org.newdawn.slick.*;
-import org.newdawn.slick.geom.Circle;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Main extends BasicGame {
-    private ArrayList<Circle> circles;
-    private int timePassed = 0;
+    private Image back;
+    private Platform platform;
+    private final Path RESOURCE = Paths.get("./resource/");
+    private Ball ball;
+    private ArrayList<GameObject> gameObjects;
 
     public Main(String title) {
         super(title);
-        circles = new ArrayList<Circle>();
+
     }
 
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        circles.add(new Circle(gameContainer.getInput().getMouseX(),
-                gameContainer.getInput().getMouseY(), 10));
+        gameObjects = new ArrayList<>();
+        try {
+            back = new Image(String.valueOf(RESOURCE.resolve("forest_back.png"))).getScaledCopy(640, 480);
+            Image bl = new Image(String.valueOf(RESOURCE.resolve("orb.png")));
+            Image pl = new Image(String.valueOf(RESOURCE.resolve("platform.png")));
+            pl = pl.getScaledCopy(2.4f);
+            pl.rotate(90f);
+
+            platform = new Platform(10, 20, pl.getWidth(), pl.getHeight(), pl);
+            ball = new Ball(200, 200, bl.getWidth(), bl.getHeight(), bl);
+            gameObjects.add(platform);
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+        gameContainer.setTargetFrameRate(60);
+        gameContainer.setAlwaysRender(true);
+        gameContainer.setVSync(true);
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int delta) {
+        var inp = gameContainer.getInput();
+        if (inp.isKeyPressed(Input.KEY_S)) {
+            platform.move(gameContainer, delta, 1);
+        }
+        if (inp.isKeyPressed(Input.KEY_W)) {
+            platform.move(gameContainer, delta, -1);
+        }
+        if (!ball.isCollision(ball.getX(), ball.getY(), gameObjects)) {
+            ball.move(gameContainer, (int) (delta * 0.4f));
+        }
 
 
     }
 
     @Override
-    public void update(GameContainer gameContainer, int delta) throws SlickException {
-        timePassed += delta;
-        if (timePassed > 500) {
-            timePassed = 0;
-            circles.add(new Circle(300, 0, 10));
+    public void render(GameContainer gameContainer, Graphics graphics) {
 
-        }
-        for (Circle circle : circles) {
-
-            circle.setCenterX(circle.getCenterX() + (delta * 0.1f));
-            circle.setCenterY(circle.getCenterY() + (delta * 0.1f));
-        }
-        for (int i = 0; i < circles.size(); i++) {
-            Circle circ = circles.get(i);
-            if (circ.getCenterX() > gameContainer.getWidth()
-            || circ.getCenterY() > gameContainer.getHeight()) {
-                circles.remove(i);
-                System.out.println(circles.size());
-            }
-        }
-
-
-    }
-
-    @Override
-    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        Random random = new Random(System.nanoTime());
-
-        Color[] colors = {Color.cyan, Color.darkGray, Color.orange, Color.yellow};
-        for (Circle circle : circles) {
-            graphics.setColor(colors[random.nextInt(colors.length)]);
-            graphics.fill(circle);
-
-        }
+        graphics.drawImage(back, 0, 0);
+        graphics.drawString("eeeeee", 40, 180);
+        ball.draw(graphics);
+        platform.draw(graphics);
 
     }
 
@@ -67,5 +72,74 @@ public class Main extends BasicGame {
         AppGameContainer container = new AppGameContainer(new Main("State Base"));
         container.setDisplayMode(640, 480, false);
         container.start();
+    }
+}
+
+
+class Platform extends GameObject {
+    public Platform(int x, int y, int width, int height, Image sprite) {
+        super(x, y, width, height, sprite);
+    }
+
+    public void move(GameContainer gameContainer, int speed, int direction) {
+        if (y >= 0 && y <= gameContainer.getHeight()) {
+            y += speed * direction;
+        }
+    }
+}
+
+
+class Ball extends GameObject {
+    int directionX = -1;
+    int directionY = -1;
+
+
+    public Ball(int x, int y, int width, int height, Image sprite) {
+        super(x, y, width, height, sprite);
+
+    }
+
+    public void move(GameContainer gameContainer, int speed) {
+        if (x <= 0) {
+            directionX = 1;
+        }
+        if (x >= gameContainer.getWidth() - getWidth()) {
+            directionX = -1;
+        }
+        if (y <= 0) {
+            directionY = 1;
+        }
+        if (y >= gameContainer.getHeight() - getHeight()) {
+            directionY = -1;
+        }
+        x += speed * directionX;
+        y += speed * directionY;
+    }
+
+
+    private int getCenterX() {
+        return (x + width) / 2;
+    }
+
+    private int getCenterY() {
+        return (y + height) / 2;
+    }
+
+    @Override
+    public void wallCollision(GameObject object) {
+        int minY = object.getY();
+        int maxY = object.getY() + object.getHeight();
+        int center = (maxY - minY) / 2;
+        if (getCenterY() > center) {
+            directionX = 1;
+            directionY = 1;
+        } else if (getCenterY() < center) {
+            directionY = -1;
+            directionX = 1;
+        } else {
+            directionX = 1;
+            directionY = 1;
+        }
+
     }
 }
